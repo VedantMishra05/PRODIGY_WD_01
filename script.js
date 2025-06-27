f0df6edefae5f08570e890f57ee70be32a5cf80c
VedantMishra05 <mishrasandhya0364@gmail.com>
2025-06-28 00:37:03 +0530

    first_task_complete

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change navbar color on scroll
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.style.backgroundColor = '#000'; // darker shade when scrolling
+  } else {
+    navbar.style.backgroundColor = '#333'; // original color
+  }
+});
